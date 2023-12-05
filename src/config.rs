36b98418117fc92379b7a2b07384c48c15697c05
AtleSkaanes <atle.skaanes@gmail.com>
2023-12-05 22:13:00 +0100

    Changed .ron location

--- src/config.rs
+++ src/config.rs
@@ -5,6 +5,8 @@ use std::{
     path::{Path, PathBuf},
 };
 
+const FILE_NAME: &str = "configure.ron";
+
 use directories::UserDirs;
 use serde::{Deserialize, Serialize};
 
@@ -14,7 +16,7 @@ pub fn get_config() -> ConfigOpts {
     match open_config() {
         Ok(content) => {
             return ron::from_str::<ConfigOpts>(&content)
-                .expect("\"sort.ron\" should be in valid ron format")
+                .expect(&format!("\"{}\" should be in valid ron format", FILE_NAME))
         }
         Err(_) => {
             let _ = create_config();
@@ -50,7 +52,7 @@ pub struct ConfigOpts {
 
 pub fn open_config() -> std::io::Result<String> {
     let dir = data::get_data_path().expect("Should have a data path");
-    fs::read_to_string(dir.join("sort.ron"))
+    fs::read_to_string(dir.join(FILE_NAME))
 }
 
 pub fn create_config() -> std::io::Result<()> {
@@ -58,7 +60,7 @@ pub fn create_config() -> std::io::Result<()> {
 
     fs::create_dir_all(dir)?;
 
-    let mut f = fs::File::create(dir.join("sort.ron"))?;
+    let mut f = fs::File::create(dir.join(FILE_NAME))?;
 
     let contents =
         ron::ser::to_string_pretty(&get_default_config(), ron::ser::PrettyConfig::default())
